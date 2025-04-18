c20f026b2bd2677e4107bd46c0123841144fcfda
ItzDaniell <juan.rodriguez.o@tecsup.edu.pe>
2025-04-18 11:20:41 -0500

    Update Form

---
+++ src/FastConvert/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+
+class YouTubeDownloadForm(forms.Form):
+    URL = forms.URLField(
+        label='URL del Video',
+        widget=forms.TextInput(attrs={
+            'class': 'input-form',
+            'placeholder': 'Introduce aquí la URL del video'
+        })
+    )
+    tipo = forms.ChoiceField(
+        choices=[('MP3', 'MP3'), ('MP4', 'MP4')],
+        widget=forms.Select(attrs={'class': 'select-form'})
+    )
